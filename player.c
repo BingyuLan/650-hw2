15471b14afce26487c3032f32a7ce77afec35146
Lan Bingyu <bl161@duke.edu>
2017-02-18 15:21:44 -0500

    make an array of name

--- player.c
+++ player.c
@@ -1,13 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/select.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
 #include "potato.h"
 
-int main(int argc, char *argv[])
-{
-  exit(0);
+int open_master_player_fifo(int id){
+  char name[20];
+  snprintf(name, sizeof(name), "master_p%d", id);
+  char path[30] = "/tmp/";
+  strcat(path, name);
+  int master_p = open(path, O_RDONLY);
+  //add error checking
+  return master_p;
+}
+
+int open_player_master_fifo(int id){
+  char name[20];
+  snprintf(name, sizeof(name), "p%d_master", id);
+  char path2[30] = "/tmp/";
+  strcat(path2, name);
+  int p_master = open(path2, O_WRONLY);
+  return p_master;
+}
+
+int main(int argc, char *argv[]){
+  //  exit(0);
+  if(argc != 2){
+    fprintf(stderr, "player <player_id>\n");
+    return EXIT_FAILURE;
+  }
+  int id = atoi(argv[1]);
+
+  int pp_fifo = 0;
+  
+  int master_p = open_master_player_fifo(id);
+  int p_master = open_player_master_fifo(id);
+  POTATO_T * potato;
+  POTATO_T p;
+  potato = &p;
+
+    
+  fd_set rfds;
+  FD_ZERO(&rfds);
+  FD_SET(master_p, &rfds);
+  int retval;
+  
+  retval = select(master_p+1, &rfds, NULL, NULL, NULL);
+  if(retval == -1){
+    perror("select()");
+  }
+  else if(retval){
+    if(FD_ISSET(master_p, &rfds)){
+      if(read(master_p, potato, sizeof(*potato)) == sizeof(*potato)){
+	printf("total_hops = %d\n", potato->total_hops);
+	printf("hops_count = %d\n", potato->hop_count);
+	printf("id = %d\n", id);
+      }
+    }	
+  }
+
+  
+  /*  
+  //open player_player pipe
+  int a = id-1;
+  if(a < 0){
+    a = players - 1;
+  }
+  int p_pl = open_player_player_fifo();
+  int pl_p = open_player_player_fifo();
+  int p_pr = open_player_player_fifo();
+  int pr_p = open_player_player_fifo();
+  */
+
+  
+  return EXIT_SUCCESS;
+
 }
--- ringmaster.c
+++ ringmaster.c
@@ -6,7 +6,63 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <unistd.h>
+#include <time.h>
 #include "potato.h"
+
+void create_master_to_players_fifo(int players, char ** pipename1){
+  //create fifo
+
+  for(int i = 0; i < players; i++){
+    char name[20];
+    snprintf(name, sizeof(name), "master_p%d", i);
+    char path[30] = "/tmp/";
+    strcat(path, name);
+    unlink(path);
+    pipename1[i] = malloc(30 * sizeof(*pipename1[i]));
+    strcpy(pipename1[i], path);
+    //make a fifo
+    if(mkfifo(path, S_IRUSR | S_IWUSR) != 0){
+      perror("mkfifo() error");
+    }
+    //printf("%s\n", name);
+
+    snprintf(name, sizeof(name), "p%d_master", i);
+    char path2[30] = "/tmp/";
+    strcat(path2, name);
+    unlink(path2);
+    if(mkfifo(path2, S_IRUSR | S_IWUSR) != 0){
+      perror("mkfifo() error");
+    }
+    //printf("%s\n", name);
+  }
+
+}
+
+void create_players_to_players_fifo(int players){
+  for(int i = 0; i < players; i++){
+    int a = i-1;
+    for(int j = 0; j < 2; j++){
+      if(a < 0){
+	a = players - 1;
+      }
+      if(a == players){
+	a = 0;
+      }
+      char name[20];
+      snprintf(name, sizeof(name), "p%d_p%d", i, a);
+      //printf("%s\n", name);
+      char path[30] = "/tmp/";
+      strcat(path, name);
+      unlink(path);
+      if(mkfifo(path, S_IRUSR | S_IWUSR) != 0){
+	perror("mkfifo() error");
+      }
+      a = i+1;
+    }
+  }
+
+}
 
 
 int main(int argc, char *argv[]){
@@ -23,57 +79,54 @@ int main(int argc, char *argv[]){
     return EXIT_FAILURE;
   }
 
-  //create fifo
-
+  char ** pipename1 = malloc((2*players) * sizeof(*pipename1));
+  
+  
+  create_master_to_players_fifo(players, pipename1);
+  create_players_to_players_fifo(players);
   for(int i = 0; i < players; i++){
-    char name[20];
-    snprintf(name, sizeof(name), "master_p%d", i);
-    char path[30] = "/tmp/";
-    strcat(path, name);
-    //make a fifo
-    if(mkfifo(path, S_IRUSR | S_IWUSR) != 0){
-      perror("mkfifo() error");
-    }
-    //printf("%s\n", name);
-
-    snprintf(name, sizeof(name), "p%d_master", i);
-    char path2[30] = "/tmp/";
-    strcat(path2, name);
-    if(mkfifo(path2, S_IRUSR | S_IWUSR) != 0){
-      perror("mkfifo() error");
-    }
-    //printf("%s\n", name);
-  }
-
-  for(int i = 0; i < players; i++){
-    int a = i-1;
-    for(int j = 0; j < 2; j++){
-      if(a < 0){
-	a = players - 1;
-      }
-      if(a == players){
-	a = 0;
-      }
-      char name[20];
-      snprintf(name, sizeof(name), "p%d_p%d", i, a);
-      //printf("%s\n", name);
-      char path[30] = "/tmp/";
-      strcat(path, name);
-      if(mkfifo(path, S_IRUSR | S_IWUSR) != 0){
-	perror("mkfifo() error");
-      }
-      a = i+1;
-    }
+    printf("%s\n", pipename1[i]);
   }
 
   //open fifo
+  int fdwrite[players];
+  int fdread[players];
   for(int i = 0; i < players; i++){
     char name[20];
     snprintf(name, sizeof(name), "master_p%d", i);
     char path[30] = "/tmp/";
     strcat(path, name);
-    
+    fdwrite[i] = open(path, O_WRONLY);
+    //add error checking
+    snprintf(name, sizeof(name), "p%d_master", i);
+    char path2[30] = "/tmp/";
+    strcat(path2, name);
+    fdread[i] = open(path2, O_RDONLY);
+  }
 
+
+  POTATO_T * potato;
+  POTATO_T p;
+  potato = &p;
+  potato->msg_type = 0; //0 means popato
+  potato->total_hops = hops;
+  potato->hop_count = 0;
+
+  srand((unsigned int) time(NULL));
+  int random = rand() % players;
+  printf("first_potato_to : %d\n", random);
+  
+  if(write(fdwrite[random], potato, sizeof(*potato)) != sizeof(*potato)){
+    fprintf(stderr, "write to file error\n");
+    return EXIT_FAILURE;
+  }      
+  else{
+    printf("pass potato success\n");
+  }
+  
+  
+  return EXIT_SUCCESS;
   
 }
+
 
